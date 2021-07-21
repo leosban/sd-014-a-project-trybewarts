9cd89f4aa339daa2aa6aa39178604341f5f2c6d2
Matheus Alves <malves224@gmail.com>
2021-07-21 16:01:28 -0300

    atualiza arquivo css

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function validarLoginSenha () {
+    function validar () {
+        
+    }
+
     const button = document.getElementById('autenticação"')
     button.addEventListener('click' validar)
 }
