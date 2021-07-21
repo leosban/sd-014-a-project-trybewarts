d2a97cac1f6c94236d9e08a00d144f34a08b360e
WalmirLucena <walmirlucena3@gmail.com>
2021-07-21 14:26:32 -0400

    Adiciona Form com input de Login,Senha e Botao de entrar

---
+++ script.js
@@ -0,0 +1,11 @@
+const email = document.getElementById('email');
+const password =document.getElementById('senha');
+const buttonLogin = document.getElementById('button-login');
+
+buttonLogin.addEventListener('click', () => {
+    if(email.value === 'tryber@teste.com' && password.value === '123456'){
+        alert('Olá, Tryber!');
+    } else{
+        alert('Login ou senha inválidos');
+    }
+})
