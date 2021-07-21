db2f361048ff56fd1327e44e01f16c4b2147298a
DanFilipe2000 <anielilipe90@gmail.com>
2021-07-21 16:32:13 -0300

    Requisito 3

---
+++ script.js
@@ -0,0 +1,13 @@
+const inputLogin = document.getElementById('login');
+const inputSenha = document.getElementById('senha');
+const butaoLogin = document.getElementById('button');
+
+butaoLogin.addEventListener('click', efetivarLogin);
+
+function efetivarLogin () {
+    if(inputLogin.value === "tryber@teste.com" && inputSenha.value === "123456"){
+        alert('Olá, Tryber!');
+    }else{
+        alert('Login ou senha inválidos.');
+    }
+}
