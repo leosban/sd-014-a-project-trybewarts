c9b0eb99124958ddd26eece77a01ca8447302c29
WalmirLucena <walmirlucena3@gmail.com>
2021-07-21 15:37:25 -0400

    adiciona botao de enviar desabilitado

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.getElementById('email');
 const password =document.getElementById('senha');
 const buttonLogin = document.getElementById('button-login');
+const agreement = document.getElementById('agreement');
+const submitBtn = document.getElementById('submit-btn');
 
 buttonLogin.addEventListener('click', () => {
     if(email.value === 'tryber@teste.com' && password.value === '123456'){
@@ -8,4 +10,9 @@ buttonLogin.addEventListener('click', () => {
     } else{
         alert('Login ou senha inválidos');
     }
-})
+});
+
+agreement.onchange = function swap() { 
+        submitBtn.disabled = !this.checked;
+  };
+
