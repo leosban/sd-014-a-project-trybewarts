65246edbe851bbb15412d319e3fb7071a2c22cd1
Lucas Freitas <lucasr.freitas@icloud.com>
2021-07-21 16:49:42 -0300

    COrrige erros do lint no javascript

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function buttonLogin() {
-    document.getElementById('login').addEventListener('click', () => {
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
-        if (username === 'tryber@teste.com' && password === '123456') {
-            alert('Olá, Tryber!');
-        } else {
-            alert('Login ou senha inválidos.');
-        }
-    });
+  document.getElementById("login").addEventListener("click", () => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    if (username === "tryber@teste.com" && password === "123456") {
+      alert("Olá, Tryber!");
+    } else {
+      alert("Login ou senha inválidos.");
+    }
+  });
 }
 
 buttonLogin();
