2e8ea7e4dbaf9148681e027e32c42810cc09f0c2
Gabriel <gabrielnmwv@gmail.com>
2021-07-21 16:07:52 -0300

    Requesito 3 + Jquery

---
+++ script.js
@@ -0,0 +1,10 @@
+let botaoLogin = document.getElementById('loginButton');
+botaoLogin.addEventListener('click', function verificaLogin(event) {
+  let inputTxtLogin = document.getElementById('login').value;
+  let inputTxtSenha = document.getElementById('senha').value;
+  if (inputTxtLogin == 'tryber@teste.com' && inputTxtSenha == '123456') {
+    alert('Olá, Tryber!')
+  } else {
+    alert('Login ou senha inválidos.')
+  }
+})
