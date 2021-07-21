de7c4f658c2667faf4215254007fdd92c7d789c8
Écio Ferraz <ecioferraz@hotmail.com>
2021-07-21 17:45:57 -0300

    lint

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const loginBtn = document.querySelector('.loginBtn');
 const loginInput = document.querySelector('#login-input');
 const passwordInput = document.querySelector('#password-input');
 
-loginBtn.addEventListener('click', function loginCheck() {
+function loginCheck() {
   if (loginInput.value !== 'tryber@teste.com' || passwordInput.value !== '123456') {
     alert('Login ou senha inválidos.');
   } else {
     alert('Olá, Tryber!');
   }
-});
+}
+
+loginBtn.addEventListener('click', loginCheck);
