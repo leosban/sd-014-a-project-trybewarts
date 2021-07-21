d6a701d1f84598973f65e1bed9f760a05931b320
Carlos Barros <carlos_dbs@hotmail.com>
2021-07-21 16:58:28 -0300

    Adiciona Validacao de login

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-const inputEmail = document.getElementById('input-email');
-const inputPassword = document.getElementById('input-password');
+const validEmail = 'tryber@teste.com';
+const validPassword = '123456';
+
+function validaLogin(email, password) {
+  const btnLogin = document.getElementById('btn-login');
+  btnLogin.addEventListener('click', () => {
+    const emailValue = document.getElementById('input-email').value;
+    const passwordValue = document.getElementById('input-password').value;
+
+    if (emailValue === email && passwordValue === password) {
+      alert('Olá, Tryber');
+    } else {
+      alert('Login ou senha inválidos');
+    }
+  });
+}
+validaLogin(validEmail, validPassword);
