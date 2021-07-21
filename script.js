826062b581f3101495593b25d72a2e0bf43b2083
Riquelme <riquelmebandeira1@outlook.com>
2021-07-21 16:18:37 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const botaoEnviar = document.querySelector("#enviar");
-botaoEnviar.addEventListener("click", validarLogin);
+const botaoEnviar = document.querySelector('#enviar');
+botaoEnviar.addEventListener('click', validarLogin);
 
 function validarLogin() {
-  const valorLogin = document.querySelector("#email").value;
-  const valorSenha = document.querySelector("#senha").value;
+  const valorLogin = document.querySelector('#login').value;
+  const valorSenha = document.querySelector('#senha').value;
 
-  if (valorLogin !== "tryber@teste.com" || valorSenha !== "123456") {
-    alert("Login ou senha inválidos.");
+  if (valorLogin !== 'tryber@teste.com' || valorSenha !== '123456') {
+    alert('Login ou senha inválidos.');
   } else {
-    alert("Olá, Tryber!");
+    alert('Olá, Tryber!');
   }
 }
