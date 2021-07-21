c350feb7ad6aa0781379bbb11bb935dd2a72bc99
Matheus Alves <malves224@gmail.com>
2021-07-21 16:19:53 -0300

    cria funçao que verifica o valor dos input email e senha

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function validarLoginSenha () {
-    function validar () {
-        const inputEmail = document.getElementById('js-email');
-        const inputSenha = document.getElementById('js-senha');
+function validarLoginSenha() {
+  function validar() {
+    const inputEmail = document.getElementById('js-email');
+    const inputSenha = document.getElementById('js-senha');
+    if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Login ou senha inválidos.');
     }
+  }
 
-    const button = document.getElementById('autenticacao')
-    button.addEventListener('click', validar)
+  const button = document.getElementById('autenticacao')
+  button.addEventListener('click', validar)
 }
+
+validarLoginSenha();
