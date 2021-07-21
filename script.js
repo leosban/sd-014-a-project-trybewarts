297c788ae024ca5a38e4d73b83e735728a3bc967
Maurício Leite <mauricioleite1@gmail.com>
2021-07-21 16:00:35 -0300

    Correções do Requisito 03

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-const btnLogar = document.getElementById('btn-logar');
+const btnLogar = document.querySelector('#btn');
 const login = document.getElementById('login').value;
 const senha = document.getElementById('senha').value;
 let loginPattern = 'tryber@teste.com';
 let senhaPattern = '123456';
 
-btnLogar.addEventListener('click', () => {
-  console.log('boraaaa');
-
-})
-
- /* if (login === loginPattern && senha === senhaPattern) {
-    window.alert = 'Olá, Tryber!'
+btnLogar.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (login === loginPattern && senha === senhaPattern) {
+    window.alert('Olá, Tryber!')
   } else {
-    window.alert = 'Login ou senha inválidos.'
+    window.alert('Login ou senha inválidos.')
   }
-}*/
+})
