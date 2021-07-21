71214d307a8d0e8ab3d7013251d81fefe2d551fd
Écio Ferraz <ecioferraz@hotmail.com>
2021-07-21 16:03:26 -0300

    requisito 3

---
+++ script.js
@@ -0,0 +1,11 @@
+const loginBtn = document.querySelector('.loginBtn');
+const loginInput = document.querySelector('#login-input');
+const passwordInput = document.querySelector('#password-input');
+
+loginBtn.addEventListener('click', function () {
+  if (loginInput.value !== 'tryber@teste.com' || passwordInput.value !== '123456') {
+    alert('Login ou senha inválidos');
+  } else {
+    alert('Olá, Tryber!')
+  }
+})
