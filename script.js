af4fefdeddbe87e799cde3ced38c80fe302f88a6
JosueTavaress <josueferreira8.jf@gmail.com>
2021-07-21 17:03:30 -0300

    adiciona requisito 18

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 function btnLogin() {
-    document.getElementById('login').addEventListener('click', () => {
-      const nomeDeUsuario = document.getElementById('nomeDeUsuario').value;
-      const senhaDeUsuario = document.getElementById('senhaDeUsuario').value;
-      if (senhaDeUsuario === '123456' && nomeDeUsuario === 'tryber@teste.com') {
-        alert('Olá, Tryber!');
-      } else {
-        alert('Login ou senha inválidos.');
-      }
-    });
-  }
+  document.getElementById('login').addEventListener('click', () => {
+    const nomeDeUsuario = document.getElementById('nomeDeUsuario').value;
+    const senhaDeUsuario = document.getElementById('senhaDeUsuario').value;
+    if (senhaDeUsuario === '123456' && nomeDeUsuario === 'tryber@teste.com') {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Login ou senha inválidos.');
+    }
+  });
+}
 
-  btnLogin()
+function desabilitaBtn() {
+  const btn = document.getElementById('submit-btn');
+  btn.setAttribute('disabled', 'disabled');
+}
+
+function habilitaBtn() {
+  document.getElementById('agreement').addEventListener('click', () => {
+    const checkObrigatorio = document.getElementById('agreement');
+    if (checkObrigatorio.checked) {
+      const estadoBtn = document.getElementById('submit-btn');
+      estadoBtn.removeAttribute('disabled', 'disabled');
+    } else {
+      desabilitaBtn();
+    }
+  });
+}
+
+window.onload = () => {
+  btnLogin();
+  desabilitaBtn();
+  habilitaBtn();
+};
