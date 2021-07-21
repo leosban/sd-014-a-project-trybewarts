9c70685ed01b341d5cd9c9f23717bf533388b3d2
Lucas Freitas <lucasr.freitas@icloud.com>
2021-07-21 16:54:06 -0300

    Corrige erro de aspas no javascript

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function buttonLogin() {
-  document.getElementById("login").addEventListener("click", () => {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    if (username === "tryber@teste.com" && password === "123456") {
-      alert("Olá, Tryber!");
+  document.getElementById('login').addEventListener('click', () => {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    if (username === 'tryber@teste.com' && password === '123456') {
+      alert('Olá, Tryber!');
     } else {
-      alert("Login ou senha inválidos.");
+      alert('Login ou senha inválidos.');
     }
   });
 }
