dfe2d3662316b38f30f41d99d00474f2e6098f38
MartinSilva <martinempreendedor@gmail.com>
2021-07-21 16:14:36 -0300

    item 3

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function addEventButtonLogin() {
+  document.getElementById('button').addEventListener('click', (event) => {
+    const t = document.getElementById('login').value === 'tryber@teste.com';
+    const s = document.getElementById('senha').value === '123456';
+    event.preventDefault();
+    if (t && s) {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Login ou senha inválidos.');
+    }
+  });
+}
+
+window.onload = () => {
+  addEventButtonLogin();
+  };
