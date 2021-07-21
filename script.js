6fcfe1135e5f0a7bf6f83396650fc112b6aee874
Anna Hamann <annasoldih@gmail.com>
2021-07-21 15:09:39 -0300

    Adiciona script de login

---
+++ script.js
@@ -0,0 +1,11 @@
+function verificaLogin() {
+  const login = document.getElementById('login');
+  const senha = document.getElementById('senha');
+
+  if (login.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+document.getElementById('gobut').addEventListener('click', verificaLogin);
