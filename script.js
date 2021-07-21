3d65581267e8b538d4166df9557e86db5c8e12a5
Maurício Leite <mauricioleite1@gmail.com>
2021-07-21 17:39:39 -0300

    Requisito 16

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const btnLogar = document.querySelector('#btn');
 const login = document.getElementById('login');
 const senha = document.getElementById('senha');
-let loginPattern = 'tryber@teste.com';
-let senhaPattern = '123456';
+const loginPattern = 'tryber@teste.com';
+const senhaPattern = '123456';
 
 btnLogar.addEventListener('click', (event) => {
   event.preventDefault();
   if (login.value === loginPattern && senha.value === senhaPattern) {
-    window.alert("Olá, Tryber!");
+    window.alert('Olá, Tryber!');
   } else {
     window.alert('Login ou senha inválidos.');
   }
-})
+});
