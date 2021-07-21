c2bba124c7745aebfd2b1cbbe699b6157c08a410
João Ferreira <joaovtf39@gmail.com>
2021-07-21 15:51:07 -0300

    add exercicio 2 e 3 parcialmente prontos

---
+++ script.js
@@ -0,0 +1,17 @@
+const btnLogar = document.getElementById('btn-logar');
+const login = document.getElementById('login').value;
+const senha = document.getElementById('senha').value;
+let loginPattern = 'tryber@teste.com';
+let senhaPattern = '123456';
+
+btnLogar.addEventListener('click', () => {
+  console.log('boraaaa');
+
+})
+
+ /* if (login === loginPattern && senha === senhaPattern) {
+    window.alert = 'Olá, Tryber!'
+  } else {
+    window.alert = 'Login ou senha inválidos.'
+  }
+}*/
