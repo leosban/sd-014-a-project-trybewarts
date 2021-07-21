02e310a53541053d403779e38e19082f98c8bddf
Williamns <williamnstolb@gmail.com>
2021-07-21 17:11:27 -0300

    Requisito completo 3

--- script.js
+++ script.js
@@ -2,13 +2,24 @@ const enviar = document.querySelector('#enviar');
 const login = document.querySelector('#login');
 const senha = document.querySelector('#senha');
 
-function botaoEnviar(evento) {
-  evento.preventDefault();
+// function botaoEnviar(evento) {
+//   evento.preventDefault();
+//   const valorLogin = login.value;
+//   const valorSenha = senha.value;
+//   if (valorLogin === 'tryber@teste.com' && valorSenha === 123456) {
+//     alert('Olá, Tryber!');
+//   }
+// }
+
+enviar.addEventListener('click', () => {
+  event.preventDefault();
   const valorLogin = login.value;
   const valorSenha = senha.value;
-  if (valorlogin === 'tryber@teste.com' && valorSenha === 123456) {
+  console.log(valorLogin);
+  console.log(valorSenha);
+  if (valorLogin === 'tryber@teste.com' && valorSenha === '123456') {
     alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
   }
-}
-
-enviar.addEventListener('click', botaoEnviar);
+});
