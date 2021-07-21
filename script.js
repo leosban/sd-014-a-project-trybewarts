8a6f2998ddec857be851258925cc8bae5e41d19e
Flavio Pires <flavioasp77@gmail.com>
2021-07-21 16:30:11 -0300

    requito 3

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const getUser = document.querySelector('#login');
 const getPassword = document.querySelector('#senha');
-const botaoLogin = document.querySelector('.btn-login');
 
-function valideLogin() {
-    getPassword.toString;
-    if(getUser === 'tryber@teste.com' && getPassword === '123456') {
+
+function valideLogin() {      
+    if(getUser.value === 'tryber@teste.com' && getPassword.value === '123456') {
         alert("Olá, Tryber!");
     } else {
         alert("Login ou senha inválidos");
     }
 }
-
+const botaoLogin = document.querySelector('.btn-login');
 botaoLogin.addEventListener('click', valideLogin);
