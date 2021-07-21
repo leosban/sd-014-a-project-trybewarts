b83ffae627740301a1f19b1bd6b9df17a443d36a
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-07-21 16:19:10 -0300

    criacao do botao login

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ const btnLogin = document.getElementById('btn-login');
 const userName = document.getElementById('login');
 const password = document.getElementById('senha');
 
-btn
+function enter(event){
+    event.prevenDefault()
+    if(userName.value === 'tryber@teste.com' && password.value === '123456'){
+        console.log('valido')
+    }
+}
+
+btnLogin.addEventListener('click',enter);
