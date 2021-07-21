cf6fbd6e587243cdcb31f293f81657f90399eafb
Michel <ferrazmichel@gmail.com>
2021-07-21 17:19:59 -0300

    Requisito 3 - JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function loginCheck () {
-  
+const loginInput = document.querySelector('#email');
+const senhaInput = document.querySelector('#senha');
+
+// Função para validar senha ... REQUISITO 3
+function senhaCheck() {
+  if (senhaInput.value !== '123456') {
+    alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
+  }
+}
+
+// Função para validar login ... REQUISITO 3
+function loginCheck() {
+  if (loginInput.value !== 'tryber@teste.com') {
+    alert('Login ou senha inválidos.');
+  } else {
+    senhaCheck.call();
+  }
 }
 
 const loginButton = document.querySelector('#login');
-loginButton.addEventListener('click', loginCheck());
+loginButton.addEventListener('click', loginCheck);
