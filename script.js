725afc491ed97ed5fa5c269b2b60c9ede1f262b7
caiquequaresmasilva <caique.quaresma@gmail.com>
2021-07-21 16:15:24 -0300

    Requisito 3 Jhonathan

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+const loginInput = document.getElementById('login');
+const senhaInput = document.getElementById('senha');
+const entrarBTN = document.getElementById('entrar');
 
-function click(params) {
-   
+function clicar() {
+  if (loginInput.value === 'tryber@teste.com' && senhaInput.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
 }
+
+entrarBTN.addEventListener('click', clicar);
