9b742c5a558f44b8f6ca6e7996529cc847512966
Robert Nascimento <robert1a7x@gmail.com>
2021-07-21 16:04:02 -0300

    requisito 4

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-//interação de login
+// interação de login
 const loginInput = document.querySelector('#Login');
 const passwordInput = document.querySelector('#Senha');
 const buttonLogin = document.querySelector('#botaoLogin');
 
-function loginInteraction () {
+function loginInteraction() {
   if (loginInput.value === 'tryber@teste.com' && passwordInput.value === '123456') {
-    alert('Olá, Tryber!')
+    alert('Olá, Tryber!');
   } else {
-    alert('Login ou senha inválidos.')
+    alert('Login ou senha inválidos.');
   }
 }
-
 
 buttonLogin.addEventListener('click', loginInteraction);
