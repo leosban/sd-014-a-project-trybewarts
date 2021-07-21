51461b18a1caef590a24cf7b7d9831a8b1b8d2cf
Lucas Freitas <lucasr.freitas@icloud.com>
2021-07-21 16:02:06 -0300

    Resolve requisito 3

---
+++ script.js
@@ -0,0 +1,13 @@
+function buttonLogin() {
+    document.getElementById('login').addEventListener('click', () => {
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        if (username === 'tryber@teste.com' && password === '123456') {
+            alert("Olá, Tryber!");
+        } else{
+            alert('Login ou senha inválidos.');
+        }
+    });
+}
+
+buttonLogin();
