80e7be7e1a3610ea4265d2948dc9c5fb5c1baffb
Anna Hamann <annasoldih@gmail.com>
2021-07-21 15:15:52 -0300

    Adiciona js para o botao de enviar

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ function verificaLogin() {
   }
 }
 document.getElementById('gobut').addEventListener('click', verificaLogin);
+
+function enableSend() {
+  const agree = document.getElementById('agreement');
+  const send = document.getElementById('submit-btn');
+  if (agree.checked === true) {
+    send.disabled = false;
+  } else {
+    send.disabled = true;
+  }
+}
+document.getElementById('agreement').addEventListener('change', enableSend);
+
