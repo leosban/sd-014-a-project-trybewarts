309ef31e8162d9ad1e10aa79392cab34af4a1477
BrianCaetano <brian-caetano@hotmail.com>
2021-07-21 17:13:16 -0300

    muda js requisito 3

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const btnLogin = document.querySelector('#btn-login');
 
-function authLog() {
+const authLog = () => {
   const login = document.querySelector('#email').value;
   const pass = document.querySelector('#password').value;
-  if (login == 'tryber@teste.com' && pass == '123456') {
+  if (login === 'tryber@teste.com' && pass === '123456') {
     window.alert('Olá, Tryber!');
   } else {
     window.alert('Login ou senha inválidos.');
   }
-}
+};
 
 btnLogin.addEventListener('click', authLog);
