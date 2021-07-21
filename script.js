3a29dac2087e806b2db49fbcb96414c28889ab33
Saulo Lima <saulolima.c@protonmail.com>
2021-07-21 16:38:21 -0300

    Requisitos 1 ao 3

---
+++ script.js
@@ -0,0 +1,12 @@
+const login = document.getElementById('login');
+const senha = document.getElementById('senha');
+const btnSub = document.getElementById('btnSubmit');
+
+btnSub.addEventListener('click', function(event) {
+    event.preventDefault();
+    if(login.value !== 'tryber@teste.com' || senha.value !== '123456'){
+        alert('Login ou senha inválidos.');
+    }else {
+        alert('Olá, Tryber!');
+    }
+})
