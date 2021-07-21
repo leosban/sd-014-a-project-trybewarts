68fcd589fd26c5e9eaa4f6aa88be32e965f1f983
Guilherme Leao <leaob7@gmail.com>
2021-07-21 16:24:31 -0300

    Requisito 3

---
+++ script.js
@@ -0,0 +1,11 @@
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+
+function validateLogin(){
+  if(email.value === 'tryber@teste.com'
+  && password.value === '123456'){
+    alert('Olá, Tryber!')
+  } else {
+    alert('Login ou senha inválidos.')
+  }
+}
