76e19cf7f47ecf9afe287c3869dc004063cd7fd3
Dogl4 <doougllas@hotmail.com.br>
2021-07-21 16:05:37 -0300

    05° Requesito - Adiciona tag main e form, dentro do body

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const botao = document.getElementById('btn-logar');
 const login = document.getElementById('login');
 const senha = document.getElementById('senha');
 
-botao.addEventListener('click', function(event) {
+botao.addEventListener('click', () => {
   if (login.value === 'tryber@teste.com' && senha.value === '123456') {
     window.alert('Olá, Tryber!');
   } else {
-    window.alert('Login ou senha inválidos.')
+    window.alert('Login ou senha inválidos.');
   }
-})
+});
