fae9366346cf8a0985ee117795c7c8840d418736
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-07-21 16:05:56 -0300

    correcoes linter

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const email = document.getElementById('email');
-const password =document.getElementById('senha');
+const password = document.getElementById('senha');
 const buttonLogin = document.getElementById('button-login');
 
 buttonLogin.addEventListener('click', () => {
-    if(email.value === 'tryber@teste.com' && password.value === '123456'){
-        alert('Olá, Tryber!');
-    } else{
-        alert('Login ou senha inválidos');
-    }
-})
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos');
+  }
+});
