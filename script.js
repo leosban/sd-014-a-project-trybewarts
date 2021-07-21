005a70a85361a085447ae91cf31ac1343d895cbe
Natália Ferreira Lima <ferlima.natalia@gmail.com>
2021-07-21 17:50:10 -0300

    requisito 4 - adiciona titulo

--- script.js
+++ script.js
@@ -2,17 +2,13 @@ function login() {
   const botaoLogar = document.getElementById('botao-logar');
 
   botaoLogar.addEventListener('click', function () {
-    let inputLogin = document.getElementById('input-login');
-    let inputSenha = document.getElementById('input-senha');
-    console.log(inputLogin.value);
-    console.log(inputSenha.value);
+    const inputLogin = document.getElementById('input-login');
+    const inputSenha = document.getElementById('input-senha');
     if (inputLogin.value === 'tryber@teste.com' && inputSenha.value === '123456') {
       alert('Olá, Tryber!');
-    }
-    else {
+    } else {
       alert('Login ou senha inválidos.');
     }
   });
 }
-
 login();
