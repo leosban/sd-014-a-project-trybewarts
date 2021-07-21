8b2195f23e8e6b929c72e6e5e6bbefa4a6750410
Gabriel <gabrielnmwv@gmail.com>
2021-07-21 17:08:13 -0300

    Commitando todas as alteracoes pro Gustavo assumir o codigo do PC dele

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let botaoLogin = document.getElementById('loginButton');
 botaoLogin.addEventListener('click', function verificaLogin(event) {
   let inputTxtLogin = document.getElementById('login').value;
   let inputTxtSenha = document.getElementById('senha').value;
-  if (inputTxtLogin == 'tryber@teste.com' && inputTxtSenha == '123456') {
+  if (inputTxtLogin == 'tryber@teste.com' | inputTxtSenha == '123456') {
     alert('Olá, Tryber!')
   } else {
     alert('Login ou senha inválidos.')
