d381db8eb0b3a056d1c47959d1343416645361c1
dandara-dias <dandaradias.contato@gmail.com>
2021-07-21 16:21:20 -0300

    Requisito 3 nao alinhado

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 let email = document.querySelectorAll("input")[0];
 let senha = document.querySelectorAll("input")[1];
+let botao = document.querySelector("button");
+
+botao.addEventListener("click", alerta);
+
+function alerta(){
+    if(email.value == "tryber@teste.com" && senha.value == 123456){
+        alert("Olá, Tryber!");
+    }else{
+        alert("Login ou senha inválidos.");
+    }
+}
