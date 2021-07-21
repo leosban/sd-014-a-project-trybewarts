644286b2d2d3fbe62738804ec357cdc6689d30fe
Victor Veloso <victorveloso13@gmail.com>
2021-07-21 16:17:00 -0300

    Faz requisito 3

---
+++ script.js
@@ -0,0 +1,15 @@
+const catchEmail = document.getElementById('login');
+const catchPassword = document.getElementById('password');
+const catchBtnLogin = document.getElementById('btn-login');
+
+function getAcess(event) {
+  console.log(catchEmail, catchPassword);
+  if (catchEmail.value === 'tryber@teste.com' && catchPassword === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    event.preventDefault();
+    alert('Login ou senha inválidos');
+  }
+}
+
+catchBtnLogin.addEventListener('click', getAcess);
