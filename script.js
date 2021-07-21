0141df87228268850639504cec1ebbd8f93da7fb
Semnome1505 <Gabriel_1505@outlook.com>
2021-07-21 16:45:21 -0300

    Modifica o script.js.

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ function botaoEnviar(evento) {
   evento.preventDefault();
   const valorLogin = login.value;
   const valorSenha = senha.value;
-  if (valorlogin === 'tryber@teste.com' && valorSenha === 123456) {
+  alert("Olá");
+  console.log(valorLogin);
+  console.log(valorSenha);
+  if (valorLogin === 'tryber@teste.com' && valorSenha === '123456') {
     alert('Olá, Tryber!');
+  }
+  else {
+    alert('Login ou senha inválidos.');
   }
 }
 
